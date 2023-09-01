6c7d2968ed126a0a7e50be56208e362f4a48b052
STEPHEN5576 <116451839+STEPHEN5576@users.noreply.github.com>
2023-09-01 16:51:46 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,16 +3,18 @@ import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 
 const Page = () => {
+    const contextVal = useContext(ThemeContext);
+    const { theme } = contextVal;
 
     return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
+        <div className={"container"+ `bg-${theme}`} id="themed-page">
+            <p id="themed-text-container" className={`txt-${theme}`}>
                 lorem ipsum dolor iterit n stuff
             </p>
             <button className="btn" id="themed-button">Themed Button</button>
             <LocalThemedBox />
         </div>
-    )
-}
+    );
+};
 
 export { Page }
